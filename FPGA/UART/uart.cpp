019eb07624b96922a12ce5e0712f4a1530eb5bb2
lean151103-ops <lean151103@gmail.com>
2026-02-09 17:03:36 +0700

    Refactor recvFrame and update UART

--- FPGA/UART/uart.cpp
+++ FPGA/UART/uart.cpp
@@ -45,31 +45,6 @@ bool uartSend(HANDLE hSerial, const uint8_t* data, uint32_t len) {
     return WriteFile(hSerial, data, len, &bytesWritten, NULL);
 }
 
-bool recvFrame(HANDLE hSerial, std::vector<uint8_t>& outFrame, DWORD timeoutMs) {
-    outFrame.assign(107, 0);
-
-    uint32_t got = 0;
-    DWORD start = GetTickCount();
-
-    while (got < (uint32_t)outFrame.size()) {
-        DWORD bytesRead = 0;
-        BOOL ok = ReadFile(hSerial,
-                           outFrame.data() + got,
-                           (DWORD)(outFrame.size() - got),
-                           &bytesRead,
-                           NULL);
-        //std::cout << "ReadFile returned " << ok << ", bytesRead=" << bytesRead << std::endl;
-        if (!ok) return false;
-        got += (uint32_t)bytesRead;     // bytesRead có thể = 0
-
-        if (GetTickCount() - start > timeoutMs) {
-            std::cerr << "Timeout receiving full Frame_data\n";
-            return false;
-        }
-    }
-    return true;
-}
-
 uint32_t crc32(const uint8_t data[], size_t length) {
     uint32_t crc = 0xFFFFFFFF;
     for (size_t i = 0; i < length; i++) {
@@ -118,6 +93,31 @@ std::vector<uint8_t> buildFrame() {
     return frame;
 }
 
+bool recvFrame(HANDLE hSerial, std::vector<uint8_t>& outFrame, DWORD timeoutMs) {
+    outFrame.assign(107, 0);
+
+    uint32_t got = 0;
+    DWORD start = GetTickCount();
+
+    while (got < (uint32_t)outFrame.size()) {
+        DWORD bytesRead = 0;
+        BOOL ok = ReadFile(hSerial,
+                           outFrame.data() + got,
+                           (DWORD)(outFrame.size() - got),
+                           &bytesRead,
+                           NULL);
+        //std::cout << "ReadFile returned " << ok << ", bytesRead=" << bytesRead << std::endl;
+        if (!ok) return false;
+        got += (uint32_t)bytesRead;     // bytesRead có thể = 0
+
+        if (GetTickCount() - start > timeoutMs) {
+            std::cerr << "Timeout receiving full Frame_data\n";
+            return false;
+        }
+    }
+    return true;
+}
+
 bool checkFrame(const std::vector<uint8_t>&recvData) {
     const uint8_t LEN_PAYLOAD = 100;
     if(recvData.size() != 2 + 1 + LEN_PAYLOAD + 4) return false;
@@ -128,31 +128,66 @@ bool checkFrame(const std::vector<uint8_t>&recvData) {
     return crc_rx == crc_calc;
 }
 
+void delay_us(long long us)
+{
+    LARGE_INTEGER freq, start, now;
+    QueryPerformanceFrequency(&freq);
+    QueryPerformanceCounter(&start);
+
+    long long ticks = us * freq.QuadPart / 1000000LL;
+
+    do {
+        QueryPerformanceCounter(&now);
+    } while ((now.QuadPart - start.QuadPart) < ticks);
+}
+
+LARGE_INTEGER qpc_freq;
+void init_timer()
+{
+    QueryPerformanceFrequency(&qpc_freq);
+}
+
+inline int64_t timestamp_ticks()
+{
+    LARGE_INTEGER t;
+    QueryPerformanceCounter(&t);
+    return t.QuadPart;
+}
+
+inline double elapsed_us(int64_t start, int64_t end)
+{
+    return (double)(end - start) * 1e6 / qpc_freq.QuadPart;
+}
+
 int main() {
-    HANDLE uart = initUART("\\\\.\\COM5", 921600);
+    init_timer();
+    HANDLE uart = initUART("\\\\.\\COM9", 921600);
     if (uart == INVALID_HANDLE_VALUE) return -1;
 
     int bad = 0;
     int ok = 0;
-    for(int i=0; i <1000; i++) {
+    for(int i=0; i <1000000; i++) {
         std::vector<uint8_t> tx_frame = buildFrame();
         PurgeComm(uart, PURGE_RXCLEAR); 
 
+        int64_t t0 = timestamp_ticks();
         if(!uartSend(uart, tx_frame.data(), tx_frame.size())) {
             std::cerr << "Failed to send data" << std::endl;
             continue;
         }
-        Sleep(5); // Wait for data to be sent and processed
+         // Wait for data to be sent and processed
         std::vector<uint8_t> rx_frame(tx_frame.size());
         if (!recvFrame(uart, rx_frame, 500)) {
             std::cerr << "Recv timeout\n";
             bad++;
             continue;
         }
+        int64_t t1 = timestamp_ticks();
+        double rtt_us = elapsed_us(t0, t1);
         // for (uint8_t b : rx_frame) {
         //     std::cout << std::hex << (int)b << " ";
         // }
-        std::cout << std::endl;
+        std::cout << "Time for 1 loopback: " << rtt_us << std::endl;
         bool frame_ok = checkFrame(rx_frame);
         if(frame_ok) ok++;
         else bad++;
