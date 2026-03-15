9c4f6681efc437edae27c26a04a1fc319506968b
lean151103-ops <lean151103@gmail.com>
2026-03-15 23:56:33 +0700

    Implement INotifyPropertyChanged in MainViewModel

--- Telemetry_Server_v2/GNDServer/Viewmodels_user/MainViewModel.cs
+++ Telemetry_Server_v2/GNDServer/Viewmodels_user/MainViewModel.cs
@@ -1,14 +1,69 @@
-﻿using GNDServer.Services_user;
-using SciChart.Charting.Model.DataSeries;
 using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
+using GNDServer.Services_user;
+using SciChart.Charting.Model.DataSeries;
 
 namespace GNDServer.Viewmodels_user
 {
-    public class MainViewModel : IDisposable
+    public class MainViewModel : IDisposable, INotifyPropertyChanged
     {
         private readonly TelemetryService _service;
         private int _xIndex = 0;
+
+        private double _currentVelocity;
+        public double CurrentVelocity
+        {
+            get => _currentVelocity;
+            set
+            {
+                if (_currentVelocity != value)
+                {
+                    _currentVelocity = value;
+                    OnPropertyChanged(nameof(CurrentVelocity));
+                }
+            }
+        }
+
+        private double _currentRPM;
+        public double CurrentRPM
+        {
+            get => _currentRPM;
+            set
+            {
+                if (_currentRPM != value)
+                {
+                    _currentRPM = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private double _currentIMT;
+        public double CurrentIMT
+        {
+            get => _currentIMT;
+            set
+            {
+                if (_currentIMT != value)
+                {
+                    _currentIMT = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private double _velocityAngle;
+        public double VelocityAngle
+        {
+            get => _velocityAngle;
+            set
+            {
+                _velocityAngle = value;
+                OnPropertyChanged();
+            }
+        }
 
         public XyDataSeries<double, double> VelocitySeries { get; private set; }
         public XyDataSeries<double, double> IMTSeries { get; private set; }
@@ -40,8 +95,20 @@ namespace GNDServer.Viewmodels_user
                 VelocitySeries.Append(_xIndex, data.Velocity);
                 IMTSeries.Append(_xIndex, data.IMT);
                 RPMSeries.Append(_xIndex, data.RPM);
+
+                CurrentVelocity = data.Velocity;
+                VelocityAngle = -120 + (CurrentVelocity / 350.0) * 240.0;
+                CurrentRPM = data.RPM;
+                CurrentIMT = data.IMT;
+
                 _xIndex++;
             }));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         public void Dispose()
