8293a1b369331edcafb6f0977a9caf65fd48cdb2
lean151103-ops <lean151103@gmail.com>
2026-03-18 17:15:00 +0700

    Refactor MainWindow to improve signal handling

--- Telemetry_Server/GND_server/MainWindow.xaml.cs
+++ Telemetry_Server/GND_server/MainWindow.xaml.cs
@@ -13,45 +13,49 @@ namespace GNDServer
         public MainWindow()
         {
             InitializeComponent();
+
             _vm = new MainViewModel();
             this.DataContext = _vm;
 
             this.Loaded += MainWindow_Loaded;
             this.Closing += MainWindow_Closing;
+
+            _vm.OnDataUpdated += Vm_OnDataUpdated;
         }
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            // 1. Gán mảng dữ liệu vào biểu đồ (Chỉ thực hiện 1 lần duy nhất)
-            VelocityPlot.Plot.AddSignal(_vm.VelocitySeries);
-            RPMPlot.Plot.AddSignal(_vm.RPMSeries);
-            IMTPlot.Plot.AddSignal(_vm.IMUSeries);
+            // ===== ADD SIGNAL =====
+            var vel = VelocityPlot.Plot.AddSignal(_vm.VelocitySeries);
+            vel.Color = System.Drawing.Color.DeepSkyBlue;
 
-            // Tùy chỉnh giao diện cho đẹp (Optional)
+            var rpm = RPMPlot.Plot.AddSignal(_vm.RPMSeries);
+            rpm.Color = System.Drawing.Color.Orange;
+
+            var imu = IMTPlot.Plot.AddSignal(_vm.IMUSeries);
+            imu.Color = System.Drawing.Color.LimeGreen;
+
             VelocityPlot.Plot.Style(ScottPlot.Style.Black);
             RPMPlot.Plot.Style(ScottPlot.Style.Black);
             IMTPlot.Plot.Style(ScottPlot.Style.Black);
 
-            // 2. QUAN TRỌNG: Đăng ký sự kiện Rendering tại đây để bắt đầu vòng lặp vẽ lại
-            CompositionTarget.Rendering += OnRendering;
+            VelocityPlot.Plot.Grid(color: System.Drawing.Color.FromArgb(40, 200, 200, 200));
+            RPMPlot.Plot.Grid(color: System.Drawing.Color.FromArgb(40, 200, 200, 200));
+            IMTPlot.Plot.Grid(color: System.Drawing.Color.FromArgb(40, 200, 200, 200));
         }
 
-        private void OnRendering(object sender, EventArgs e)
+        private void Vm_OnDataUpdated()
         {
-            // Hàm này sẽ chạy liên tục theo tần số quét màn hình (60fps hoặc hơn)
-            if (_vm != null)
+            Dispatcher.Invoke(() =>
             {
-                // Ra lệnh cho các Control ScottPlot vẽ lại dựa trên dữ liệu mảng mới nhất
-                VelocityPlot?.Render();
-                RPMPlot?.Render();
-                IMTPlot?.Render();
-            }
+                VelocityPlot.Render();
+                RPMPlot.Render();
+                IMTPlot.Render();
+            });
         }
 
         private void MainWindow_Closing(object sender, CancelEventArgs e)
         {
-            CompositionTarget.Rendering -= OnRendering;
-
             if (_vm != null)
                 _vm.Dispose();
         }
