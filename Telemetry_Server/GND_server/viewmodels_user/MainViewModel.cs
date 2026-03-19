5fc4214a426795a3d1527110ffb64acd80376151
lean151103-ops <lean151103@gmail.com>
2026-03-19 23:57:21 +0700

    Refactor MainViewModel to use new telemetry service

--- Telemetry_Server/GND_server/viewmodels_user/MainViewModel.cs
+++ Telemetry_Server/GND_server/viewmodels_user/MainViewModel.cs
@@ -2,48 +2,75 @@ using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows;
-using GNDServer.Services_user;
+using GNDServer_v2.Services_user;
 
-namespace GNDServer.Viewmodels_user
+namespace GNDServer_v2.Viewmodels_user
 {
     public class MainViewModel : IDisposable, INotifyPropertyChanged
     {
         private readonly TelemetryService _service;
 
-        // --- GIỮ NGUYÊN TÊN BIẾN CŨ CỦA BẠN ---
+        public double[] VelocityValues { get; } = new double[300];
+        public double[] RPMValues { get; } = new double[300];
+        public double[] IMUValues { get; } = new double[300];
+
         private double _currentVelocity;
         public double CurrentVelocity
         {
             get => _currentVelocity;
-            set { _currentVelocity = value; OnPropertyChanged(); }
+            set
+            {
+                if (_currentVelocity != value)
+                {
+                    _currentVelocity = value;
+                    OnPropertyChanged();
+                }
+            }
         }
 
         private double _currentRPM;
         public double CurrentRPM
         {
             get => _currentRPM;
-            set { _currentRPM = value; OnPropertyChanged(); }
+            set
+            {
+                if (_currentRPM != value)
+                {
+                    _currentRPM = value;
+                    OnPropertyChanged();
+                }
+            }
         }
 
         private double _currentIMU;
         public double CurrentIMU
         {
             get => _currentIMU;
-            set { _currentIMU = value; OnPropertyChanged(); }
+            set
+            {
+                if (_currentIMU != value)
+                {
+                    _currentIMU = value;
+                    OnPropertyChanged();
+                }
+            }
         }
 
         private double _velocityAngle;
         public double VelocityAngle
         {
             get => _velocityAngle;
-            set { _velocityAngle = value; OnPropertyChanged(); }
+            set
+            {
+                if (_velocityAngle != value)
+                {
+                    _velocityAngle = value;
+                    OnPropertyChanged();
+                }
+            }
         }
 
-        // --- THAY ĐỔI: Dùng mảng double để ScottPlot có thể vẽ ---
-        // Tôi vẫn giữ tên "Series" nếu bạn muốn, nhưng kiểu dữ liệu phải là double[]
-        public double[] VelocitySeries { get; } = new double[300];
-        public double[] RPMSeries { get; } = new double[300];
-        public double[] IMUSeries { get; } = new double[300];
+        public event EventHandler PlotsUpdated;
 
         public MainViewModel()
         {
@@ -54,27 +81,25 @@ namespace GNDServer.Viewmodels_user
 
         private void Service_OnDataReceived(TelemetryData data)
         {
-            // Cập nhật dữ liệu từ luồng UDP/C++ sang UI Thread
             Application.Current.Dispatcher.BeginInvoke(new Action(() =>
             {
-                // 1. Hiệu ứng cuộn biểu đồ (Dịch chuyển mảng)
-                Array.Copy(VelocitySeries, 1, VelocitySeries, 0, VelocitySeries.Length - 1);
-                VelocitySeries[VelocitySeries.Length - 1] = data.Velocity;
+                Push(VelocityValues, data.Velocity);
+                Push(RPMValues, data.RPM);
+                Push(IMUValues, data.IMU);
 
-                Array.Copy(RPMSeries, 1, RPMSeries, 0, RPMSeries.Length - 1);
-                RPMSeries[RPMSeries.Length - 1] = data.RPM;
-
-                Array.Copy(IMUSeries, 1, IMUSeries, 0, IMUSeries.Length - 1);
-                IMUSeries[IMUSeries.Length - 1] = data.IMU;
-
-                // 2. Cập nhật các con số hiển thị (Binding)
                 CurrentVelocity = data.Velocity;
                 CurrentRPM = data.RPM;
                 CurrentIMU = data.IMU;
-
-                // 3. Tính toán góc kim đồng hồ (Dải đo 0-350 tương ứng -120 đến +120 độ)
                 VelocityAngle = -120 + (CurrentVelocity / 350.0) * 240.0;
+
+                PlotsUpdated?.Invoke(this, EventArgs.Empty);
             }));
+        }
+
+        private static void Push(double[] values, double newValue)
+        {
+            Array.Copy(values, 1, values, 0, values.Length - 1);
+            values[values.Length - 1] = newValue;
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
