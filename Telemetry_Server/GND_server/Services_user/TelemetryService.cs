deebf499251ce850646d8e94c17d730b9ce8112c
AnLD <lean151103@gmail.com>
2026-04-09 23:55:20 +0700

    update telemetryservice

--- Telemetry_Server/GND_server/Services_user/TelemetryService.cs
+++ Telemetry_Server/GND_server/Services_user/TelemetryService.cs
@@ -4,13 +4,13 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Markup;
 
-namespace GNDServer2.Services_user
+namespace GNDServer.Services_user
 {
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
     public struct TelemetryData
     {
         public ushort lapCounter;
-        public uint   lapTime;
+        public uint lapTime;
         public ushort distance;
         public ushort speed;
         public ushort throttle;
@@ -44,42 +44,41 @@ namespace GNDServer2.Services_user
         private ushort _prevDistance;
 
         //Parameters of F1-car for simulating
-        const short m = 230;            //kg
+        const short m = 800;            //kg
         const float L = 1.53f;          //m
         const float r_w = 0.203f;       //m
         const float muy = 1.5f;         // Hệ số ma sát lốp
         const float G_final = 3.5f;
-        const float G_gear = 2.1f;  
-        const float h_break = 0.15f;     // J/(s*oC)
+        const float G_gear = 2.1f;
+        const float h_break = 150.0f;     // W/oC
         const float m_break_disc = 1.2f; // kg
         const float C_steel = 480f;      // J/(kg*K)
-        const float a_brake_max = muy * 9.81f; 
+        const float a_brake_max = muy * 9.81f;
         const float F_brake_max = muy * m * 9.81f;
         double[] gearRatio = { 0, 2.85, 2.05, 1.62, 1.35, 1.15, 1.00 }; // Gear ratios for 1st to 6th gear
 
         //Parameters of F1-Tires
-        const float m_tire = 8.0f; //kg
+        const float m_tire = 8.0f;  //kg
         const float C_tire = 1500f; // J/(kgK)
-        const float h_tire = 0.05f;
+        const float h_tire = 20.0f; // W/oC
         const float F_z = m * 9.81f / 4;
         const float muy_roll = 0.015f;
         const float k_slip = 0.1f;
 
         // ── Fuel parameters ──
         const float rpm_max = 12000f;
-        const float max_drain_rate = 0.05f;  
-        private float _fuelLevel = 100f;    
+        const float max_drain_rate = 0.5f;
 
         public event Action<TelemetryData> OnDataReceived;  // Event triggered to sending data to which need data 
 
         // P/Invoke declarations for native DLL functions
-        [DllImport("Telemetry_Native2.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("Telemetry_Native.dll", CallingConvention = CallingConvention.Cdecl)]
         private static extern int startServer();
 
-        [DllImport("Telemetry_Native2.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("Telemetry_Native.dll", CallingConvention = CallingConvention.Cdecl)]
         private static extern int stopServer();
 
-        [DllImport("Telemetry_Native2.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("Telemetry_Native.dll", CallingConvention = CallingConvention.Cdecl)]
         private static extern int getLastData(byte[] buffer, int bufferSize, out uint version);
 
 
@@ -135,7 +134,8 @@ namespace GNDServer2.Services_user
 
         private static float T_disc = 30.0f;
         private static float T_tire = 30.0f;
-        private const float T_ambient = 30.0f; 
+        private const float T_ambient = 30.0f;
+        private static float _fuelLevel = 100.0f;
 
         // Converts a byte array to a TelemetryData struct using GCHandle to pin the array in memory
         private TelemetryData Parse(byte[] buffer)
@@ -144,12 +144,12 @@ namespace GNDServer2.Services_user
             if (_thermalTimer.IsRunning)
             {
                 dt = (float)_thermalTimer.Elapsed.TotalSeconds;
-                dt = Math.Max(0.003f, Math.Min(dt, 0.05f));
+                dt = Math.Max(0.003f, Math.Min(dt, 0.1f));
                 _lastDt = dt;
             }
             else
             {
-                dt = _lastDt; 
+                dt = _lastDt;
             }
             _thermalTimer.Restart();
 
@@ -165,11 +165,11 @@ namespace GNDServer2.Services_user
             float calculatedSteering = (float)(((int)rawSteering - 500) * 35.0 / 500.0);
 
             // Tính gia tốc dọc (IMU X) trước khi cập nhật _prevSpeed
-            float simulatedImuX = (float)(rawSpeedMs - _prevSpeed/3.6f)/dt;
+            float simulatedImuX = (float)(rawSpeedMs - _prevSpeed / 3.6f) / dt;
             Console.Write($"{dt} -");
             // Tính gia tốc ngang (IMU Y)
             float steeringRadian = calculatedSteering * (float)(Math.PI / 180.0);
-            float simulatedImuY = (float)(Math.Tan(steeringRadian) * rawSpeedMs * rawSpeedMs/L);
+            float simulatedImuY = (float)(Math.Tan(steeringRadian) * rawSpeedMs * rawSpeedMs / L);
 
             // Giả lập Gear và RPM
             ushort simulatedGear = (ushort)(
@@ -186,17 +186,19 @@ namespace GNDServer2.Services_user
             if (simulatedRpm > 12000) simulatedRpm = 12000;
 
             // Tính lực phanh và nhiệt độ 
-            ushort simulatedBrake = (ushort)(rawSpeed < _prevSpeed ? Math.Abs(simulatedImuX)*100/a_brake_max : 0);
+            ushort simulatedBrake = (ushort)(rawSpeed < _prevSpeed ? Math.Abs(simulatedImuX) * 100 / a_brake_max : 0);
+            if (simulatedBrake > 100) simulatedBrake = 100;
+
             float brakeForce = (simulatedBrake / 100.0f) * F_brake_max;
             float brakeForce_disc = brakeForce / 4.0f;
-            float delta_Temp_Brake = ( brakeForce_disc * rawSpeedMs - h_break*(T_disc - T_ambient)) * dt / (m_break_disc * C_steel); 
-            ushort simulatedTempBrake = (ushort)(T_disc + delta_Temp_Brake);
-            T_disc = simulatedTempBrake; // Update the disc temperature
+            float delta_Temp_Brake = (brakeForce_disc * rawSpeedMs - h_break * (T_disc - T_ambient)) * dt / (m_break_disc * C_steel);
+            T_disc += delta_Temp_Brake;
+            ushort simulatedTempBrake = (ushort)T_disc;
 
             // Simulation cho lốp xe
-            float delta_Temp_Tire = (muy_roll * F_z * rawSpeedMs + k_slip * brakeForce_disc * rawSpeedMs - h_tire * (T_tire - T_ambient)) * dt / (m_tire*C_tire);
-            ushort simulatedTempTire = (ushort)(T_tire + delta_Temp_Tire);
-            T_tire = simulatedTempTire;
+            float delta_Temp_Tire = (muy_roll * F_z * rawSpeedMs + k_slip * brakeForce_disc * rawSpeedMs - h_tire * (T_tire - T_ambient)) * dt / (m_tire * C_tire);
+            T_tire += delta_Temp_Tire;
+            ushort simulatedTempTire = (ushort)T_tire;
 
             // Giả lập xăng (Dùng rawDistance vừa đọc)
             float rpmNorm = simulatedRpm / rpm_max;        // 0~1
@@ -205,6 +207,8 @@ namespace GNDServer2.Services_user
             _fuelLevel -= fuelRate * dt;
             _fuelLevel = Math.Max(0f, _fuelLevel);             // không âm
             ushort simulatedFuel = (ushort)_fuelLevel;          // 0~100
+            Console.WriteLine($"DEBUG SIMULATION:Delta_Temp_Brake={delta_Temp_Brake} | Delta_Temp_Tire={delta_Temp_Tire}  | Fuel={simulatedFuel}");
+
 
             // Cập nhật tốc độ cũ cho lần sau
             _prevSpeed = rawSpeed;
